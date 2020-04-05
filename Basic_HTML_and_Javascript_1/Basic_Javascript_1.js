f53f2e8db3d858ab23b7293d6b03307778dd8da6
CCordova88 <62787560+CCordova88@users.noreply.github.com>
2020-04-04 20:00:19 -0600

    Add Basic HTML and Javascript files

---
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -0,0 +1,11 @@
+alert("You are now descending deep into the cold void, passed the point of no return...");
+
+	
+
+	
+function My_First_Function() {
+	var String = "Your soul is mine!";
+	var result = String.fontcolor("green");
+	document.getElementById("ShangTsung").innerHTML =
+	result;
+	}
